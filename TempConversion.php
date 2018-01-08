bfbb7a90392306b4eb4909ac2284fea3e6635e10
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2018-01-07 17:38:09 -0800

    updateZ

---
+++ TempConversion.php
@@ -0,0 +1,50 @@
+<?php
+
+$radioVal = $_POST["Temp"];
+$varOne = '<form action="" method="post">
+<input type="radio" name="Temp" value="FtoC" checked>Fahrenheit to Celsius<br> 
+<input type="radio" name="Temp" value="CtoF">Celsius to Fahrenheit<br>
+<input type="radio" name="Temp" value="FtoK">Fahrenheit to Kelvin<br>
+<label>
+Your Number: <br>
+ <input
+  type="text"
+  name="Num"
+  placeholder="Enter a number"
+  required="required"
+  size="30"
+  maxlength="60"
+  autofocus
+  title="Number is is Required"
+  tabindex="10"
+  />
+</label>
+ <br>
+<input type="submit" />
+</form>';
+
+if (is_numeric($_POST['Num'])) 
+{
+
+if(isset($_POST['Num']) and $radioVal == "FtoC")
+{
+ echo '<h2>' . $_POST['Num'] . ' Fahrenheit is ' .  round(($_POST['Num'] - 32)/1.8) . ' Celsius. </h2><br><hr><br>' . $varOne;
+}
+
+elseif(isset($_POST['Num']) and $radioVal == "CtoF")
+{
+ echo '<h2>' . $_POST['Num'] . ' Celsius is ' .  round(($_POST['Num'] * 1.8 + 32)) . ' Fahrenheit. </h2><br><hr><br>' . $varOne;
+}
+
+elseif(isset($_POST['Num']) and $radioVal == "FtoK")
+{
+ echo '<h2>' . $_POST['Num'] . ' Fahrenheit is ' .  round(($_POST['Num'] + 459.67) * 5/9) . ' Kelvin. </h2><br><hr><br>' . $varOne;  
+
+}else{
+echo $varOne;
+} 
+
+}else{
+echo '<h2>Please enter numbers only.</h2><hr>' . $varOne;
+}
+ ?>
--- form2.php
+++ form2.php
@@ -7,11 +7,51 @@ if not, show the form */
 
 if(isset($_POST['FirstName']))
 {
-  echo $_POST['FirstName'];
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre>';
 
 }else{
 echo '<form action="" method="post">
-First Name: <input type="text" name="FirstName /"> <br>
+<label>
+First Name: <br>
+ <input
+  type="text"
+  name="FirstName"
+  placeholder="Put First Name Here"
+  required="required"
+  size="30"
+  maxlength="60"
+  autofocus
+  title="First Name is Required"
+  tabindex="10"
+  />
+</label>
+
+<br>
+
+<label>
+Last Name: <br>
+ <input
+  type="text"
+  name="LastName"
+  placeholder="Put Last Name Here"
+  required="required"
+  size="30"
+  maxlength="60"
+  autofocus
+  title="Last Name is Required"
+  tabindex="20"
+  />
+</label>
+
+<label> <br>
+    <input type="radio" name="FavColor" value="red" />Red <br>
+    <input type="radio" name="FavColor" value="blue" />Blue<br>
+    <input type="radio" name="FavColor" value="yellow" />Yellow<br>
+</label>
+
+ <br>
 <input type="submit" />
 
 
